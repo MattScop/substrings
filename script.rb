12f38887432398ee315c2a9b966583f06eaad122
MattScop <matteo.scopece@outlook.com>
2022-08-27 09:55:55 +0200

    Add step 4. to make the program dynamic

--- script.rb
+++ script.rb
@@ -3,12 +3,17 @@
 2. For each dictionary word, scan the string to see whether it is included 
 in one of the words in the dictionary array and place it inside a new array.
 3. Reduce the new array and return a hash containing a list of the words
+
+4. ADDITIONAL STEP: Make it so that the user inserts the string
 =end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 # 1.
-def substrings(string, dictionary)
+def substrings(dictionary)
+    # 4.
+    puts "Type a Phrase, I will return a Hash"
+    string = gets.chomp
     # 2.
     matched_words = []
     dictionary.each do |word|
@@ -26,7 +31,9 @@ def substrings(string, dictionary)
         end
         total
     end
-    puts new_hash
+    if new_hash == {} then puts "Sorry, your phrase did not match any word in the dictionary, try again!"; substrings(dictionary)
+    else puts new_hash
+    end
 end
 
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+substrings(dictionary)
